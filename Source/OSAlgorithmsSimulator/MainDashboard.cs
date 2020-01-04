23177ccc1946cfb064e6e577c49786e2b78a9acc
Ahmed Omar <dodosherkawy@gmail.com>
2020-01-05 00:50:18 +0200

    Blank Windows Form app ready to go!

---
+++ Source/OSAlgorithmsSimulator/MainDashboard.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace OSAlgorithmsSimulator
+{
+	public partial class MainDashboard : Form
+	{
+		public MainDashboard()
+		{
+			InitializeComponent();
+		}
+	}
+}
