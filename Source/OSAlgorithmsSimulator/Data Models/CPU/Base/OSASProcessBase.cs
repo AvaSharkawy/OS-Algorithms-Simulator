6a3f27d19fb778748f8add773a3e93879b8cc506
Ahmed Omar <dodosherkawy@gmail.com>
2020-01-06 05:29:31 +0200

    Added Data model classes
    Class to represent our process

---
+++ Source/OSAlgorithmsSimulator/Data Models/CPU/Base/OSASProcessBase.cs
@@ -0,0 +1,24 @@
+﻿namespace OSAlgorithmsSimulator
+{
+	/// <summary>
+	/// A base class to represent OSAS Process
+	/// </summary>
+	public class OSASProcessBase
+	{
+		/// <summary>
+		/// The name of this process
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// The burst time for this process
+		/// </summary>
+		public int BurstTime { get; set; }
+
+		/// <summary>
+		/// The arrival time of this process
+		/// </summary>
+		public int ArrivalTime { get; set; }
+
+	}
+}
---
+++ Source/OSAlgorithmsSimulator/Data Models/CPU/OSASProcess.cs
@@ -0,0 +1,43 @@
+﻿namespace OSAlgorithmsSimulator
+{
+	/// <summary>
+	/// A class to represent our process that we will work on
+	/// </summary>
+	public class OSASProcess : OSASProcessBase
+	{
+		/// <summary>
+		/// The Id of this process
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// The priority level of this process
+		/// </summary>
+		public int Priority { get; set; }
+
+		/// <summary>
+		/// The start time of this process
+		/// </summary>
+		public int StartTime { get; set; }
+
+		/// <summary>
+		/// The finish time of this process
+		/// </summary>
+		public int FinishTime { get; set; }
+
+		/// <summary>
+		/// The time estimated before last start time
+		/// </summary>
+		public int WaitTime { get; set; }
+
+		/// <summary>
+		/// The estimated time from arrival time to finish
+		/// </summary>
+		public int TurnAroundTime { get; set; }
+
+		/// <summary>
+		/// Indicates if this process currently working
+		/// </summary>
+		public bool IsWorking { get; set; }
+	}
+}
